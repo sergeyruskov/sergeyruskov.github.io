9d7b7b2a27b5b6cac8767df2c665fa7968dd82e7
Sergey Ruskov <sruskov@hsmedia.ru>
2016-01-21 15:23:52 +0300

    changes

--- src/php/lego/test-section/test-section-main.php
+++
@@ -1,7 +0,0 @@
-<main class="test-section-main">
-
-	<? req_with_param(PATH__PHP_LEGO . "/test-section/test-section-content.php"); ?>
-
-	<? req_with_param(PATH__PHP_COMMON . "/footer/footer-top.php"); ?>
-
-</main>
